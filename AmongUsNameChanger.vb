7af38f65a7c3fb33bfd26f3ea95e8773b8722d4b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-04-08 00:27:16 +0200

    added loading settings data from file

--- AmongUsNameChanger.vb
+++ AmongUsNameChanger.vb
@@ -14,7 +14,7 @@ Public Class AmongUsNameChanger
         settingsFile = Path.Combine(settingsFolder, "playerPrefs")
 
         If Directory.Exists(settingsFolder) Then
-            lblSettingsFolder.Text = "Found!"
+            lblSettingsFolder.Text = "Found"
         Else
             lblSettingsFolder.Text = "Not Found!"
             lblSettingsFile.Text = "Folder not found!"
@@ -25,7 +25,7 @@ Public Class AmongUsNameChanger
         End If
 
         If File.Exists(settingsFile) Then
-            lblSettingsFile.Text = "Found! Reading..."
+            lblSettingsFile.Text = "Found. Reading..."
         Else
             lblSettingsFile.Text = "Not Found!"
             MessageBox.Show("Found the settings folder, but not the file! Please launch the game first to create the file",
@@ -35,15 +35,32 @@ Public Class AmongUsNameChanger
 
         Try
             Await LoadSettings(settingsFile)
+            lblSettingsFile.Text = "Found & Loaded"
             grpName.Enabled = True
             grpDate.Enabled = True
         Catch ex As Exception
+            lblSettingsFile.Text = "Found! Error loading!"
             MessageBox.Show(ex.Message, "Error loading file", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 
     Private Async Function LoadSettings(filePath As String) As Task
+        ' we only care about the first line
+        Dim line As String
+        Using reader As StreamReader = File.OpenText(filePath)
+            line = Await reader.ReadLineAsync()
+        End Using
 
+        Dim arr As String() = line.Split(","c)
+        If arr.Length < 26 Then Throw New InvalidDataException("Settings file corrupt or old game version!")
+
+        txtName.Text = arr(0)
+
+        Dim birthDay As Integer = Integer.Parse(arr(24))
+        Dim birthMonth As Integer = Integer.Parse(arr(23))
+        Dim birthYear As Integer = Integer.Parse(arr(25))
+
+        dateSelector.SetDate(New Date(birthYear, birthMonth, birthDay))
     End Function
 
     Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
